8fb503601662ec7d8a8f13ec9b32bd24422fe285
melissapennington <mlpennington9@gmail.com>
2023-10-25 20:50:52 -0500

    customers now work

--- models/customers.py
+++ models/customers.py
@@ -6,14 +6,16 @@ class Customer:
         id (int): The customer's unique identifier.
         name (str): The customer's name.
         address (str): The customer's address.
-        status (str): The customer's status (e.g., 'active' or 'inactive').
+        email (str): The customer's email address.
+        password (str): The customer's password.
     """
 
-    def __init__(self, id, name, address, status):
+    def __init__(self, id, name, address, email, password):
         self.id = id
         self.name = name
         self.address = address
-        self.status = status
+        self.email = email
+        self.password = password
 
 # Creating a new Customer object
-new_customer = Customer(1, "John Smith", "8422 Johnson Pike", "active")
+new_customer = Customer(1, "John Smith", "8422 Johnson Pike", "jsmith@email.com", "password")
